690329db3514df5380ee3a197372328bb4dd4894
Arron Hunt <arronjhunt@gmail.com>
2023-09-27 16:24:59 -0700

    Work

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import r2wc from "@r2wc/react-to-web-component";
-import World from "./components/HelloWorld";
-import "./index.css";
-import App from "./App";
 
-const WorldComponent = r2wc(World);
+import { Theme } from "@radix-ui/themes";
+import "./index.css";
+import "@radix-ui/themes/styles.css";
+
+import World from "./components/HelloWorld";
+import Table from "./components/Table";
+import AlertDialog from "./components/AlertDialog";
+
+function withTheme(WrappedComponent) {
+  return function ThemedComponent({ ...props }) {
+    return (
+      <Theme>
+        <WrappedComponent {...props} />
+      </Theme>
+    );
+  };
+}
+
+const WorldComponent = r2wc(withTheme(World));
+const TableComponent = r2wc(withTheme(Table));
+const AlertComponent = r2wc(withTheme(AlertDialog));
 
 customElements.define("hello-world", WorldComponent);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+customElements.define("my-table", TableComponent);
+customElements.define("alert-component", AlertComponent);
